563f67830089af6ca16d2736437c86a42f8771ff
Venkat <Venkat@Venkat-PC>
2021-04-06 08:56:41 +0200

    Players plays the game alternatively on board.

--- TicTacToe/src/main/java/com/bnpp/kata/tictactoe/bean/TicTacToe.java
+++ TicTacToe/src/main/java/com/bnpp/kata/tictactoe/bean/TicTacToe.java
@@ -1,14 +1,30 @@
 package com.bnpp.kata.tictactoe.bean;
 
 public class TicTacToe {
-	private char[][] board = new char[3][3];
 
-	public void play(int row, int column, char inputChar) {
-		board[row][column] = inputChar;
+	private char[][] board = new char[3][3];
+	private char currentPlayer;
+
+	public void addPlayer(int row, int column) {
+		currentPlayer = getNextPlayer();
+		board[row][column] = currentPlayer;
 
 	}
 
 	public char getPosition(int row, int column) {
 		return board[row][column];
+	}
+
+	public char getNextPlayer() {
+		if ('X' == currentPlayer) {
+
+			return 'O';
+		} else {
+			return 'X';
+		}
+	}
+
+	public char getCurrentPlayer() {
+		return currentPlayer;
 	}
 }
--- TicTacToe/src/test/java/com/bnp/kata/tictactoe/TicTacToeTest.java
+++ TicTacToe/src/test/java/com/bnp/kata/tictactoe/TicTacToeTest.java
@@ -12,8 +12,17 @@ public class TicTacToeTest {
 	@Test
 	public void playerShouldPlaceXInAnyPosition() {
 		TicTacToe ticTacToe = new TicTacToe();
-		ticTacToe.play(1, 1, 'X');
+		ticTacToe.addPlayer(1, 1);
 		assertThat(ticTacToe.getPosition(1, 1), CoreMatchers.is('X'));
+	}
+	
+	@Test
+	public void playersShouldPlayAlternatively() {
+		TicTacToe ticTacToe = new TicTacToe();
+		ticTacToe.addPlayer(0, 0);
+		assertThat(ticTacToe.getCurrentPlayer(), CoreMatchers.is('X'));
+		ticTacToe.addPlayer(1, 1);
+		assertThat(ticTacToe.getCurrentPlayer(), CoreMatchers.is('O'));
 	}
 
 }
